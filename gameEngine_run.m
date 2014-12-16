fc3962e5da11e2d2d346e7a4d9de1977d9c1a04a
Elijas <master.elijas@gmail.com>
2014-12-16 22:56:31 +0200

    removed all previous AI system. Preparing grounds for a new approach

--- gameEngine_run.m
+++ gameEngine_run.m
@@ -1,42 +1,34 @@
 function gameFlag = gameEngine_run(player1_name, player2_name, gameCount=1)
-if (isequal(player1_name, "playerAI")), player1 = @(gameState, playerNo) playerAI(gameState, playerNo);
-    else if (isequal(player1_name, "playerRandom")), player1 = @(gameState, playerNo) playerRandom(gameState, playerNo); 
-    else if (isequal(player1_name, "playerHuman")), player1 = @(gameState, playerNo) playerHuman(gameState, playerNo); 
-    else if (isequal(player1_name, "playerBoring")), player1 = @(gameState, playerNo) playerBoring(gameState, playerNo);
-else, printf("Error: Incorrect Player 1's name: %s\n", player1_name), gameFlag=-2; end end end end
-if (isequal(player2_name, "playerAI")), player2 = @(gameState, playerNo) playerAI(gameState, playerNo);
-    else if (isequal(player2_name, "playerRandom")), player2 = @(gameState, playerNo) playerRandom(gameState, playerNo); 
-    else if (isequal(player2_name, "playerHuman")), player2 = @(gameState, playerNo) playerHuman(gameState, playerNo); 
-    else if (isequal(player2_name, "playerBoring")), player2 = @(gameState, playerNo) playerBoring(gameState, playerNo);
-else printf("Error: Incorrect Player 2's name: %s\n", player2_name), gameFlag=-2; end end end end
+if      (isequal(player1_name, "playerAI")), player1 = @(gameState, playerNo) playerAI(gameState, playerNo);
+else if (isequal(player1_name, "playerRandom")), player1 = @(gameState, playerNo) playerRandom(gameState, playerNo); 
+else if (isequal(player1_name, "playerHuman")), player1 = @(gameState, playerNo) playerHuman(gameState, playerNo); 
+else if (isequal(player1_name, "playerBoring")), player1 = @(gameState, playerNo) playerBoring(gameState, playerNo);
+else    printf("Error: Incorrect Player 1's name: %s\n", player1_name), gameFlag=-2; end end end end
+if      (isequal(player2_name, "playerAI")), player2 = @(gameState, playerNo) playerAI(gameState, playerNo);
+else if (isequal(player2_name, "playerRandom")), player2 = @(gameState, playerNo) playerRandom(gameState, playerNo); 
+else if (isequal(player2_name, "playerHuman")), player2 = @(gameState, playerNo) playerHuman(gameState, playerNo); 
+else if (isequal(player2_name, "playerBoring")), player2 = @(gameState, playerNo) playerBoring(gameState, playerNo);
+else    printf("Error: Incorrect Player 2's name: %s\n", player2_name), gameFlag=-2; end end end end
 
-global AIlearn_totalIterations; AIlearn_totalIterations = gameCount; % To help make learning rate lower towards the end of learning 
-global AIlearn_currIteration;
-if isequal(player1_name, "playerAI"), playerAI_initialize(1), global player1_totalWins; end
-if isequal(player2_name, "playerAI"), playerAI_initialize(2), global player2_totalWins; end
+global isAIPlaying
+isAIPlaying = isequal(player1_name, "playerAI") || isequal(player2_name, "playerAI");
+if isAIPlaying, playerAI_initialize; end
 
 global isHumanPlaying % Will be used to turn on/off UI features
-isHumanPlaying = false;
-if isequal(player1_name, "playerHuman") || isequal(player2_name, "playerHuman"), 
-    isHumanPlaying = true;
-    printf("Instructions: Use your numpad to play\n"); 
-end
-
-
+isHumanPlaying = isequal(player1_name, "playerHuman") || isequal(player2_name, "playerHuman");
+if isHumanPlaying, printf("Instructions: Use your numpad to play\n"); end
 
 gameStatistics = [0;0;0]; % Data stored: player1 wins, player2 wins, ties
 gameStatistics_errors = 0; % Terminal states reached because of system/player failures (bugs)
 gameStatistics_results = [];
 
 for i=1:gameCount
-    AIlearn_currIteration = i; % To help make learning rate lower towards the end of learning
-
     printf("%d\r",i); if exist('OCTAVE_VERSION'), fflush(stdout); end
+    
     gameResult = gameEngine_oneGame(player1, player2);
     
-    if isequal(player1_name, "playerAI"), player1_totalWins+=(gameResult==1); playerAI_learn(gameResult, 1); end
-    if isequal(player2_name, "playerAI"), player2_totalWins+=(gameResult==2); playerAI_learn(gameResult, 2); end
-    
+    if isAIPlaying, playerAI_learn(gameResult); end
+
     if gameResult == 1, gameStatistics(1)++;
     else if gameResult == 2, gameStatistics(2)++;
     else if gameResult == -1, gameStatistics(3)++;
@@ -48,9 +40,9 @@ end
 
 if !isHumanPlaying && gameCount>1
     close all;
-    figureID = 3;
     
-    printf("%s: %d, %s: %d, ties: %d\n", player1_name, gameStatistics(1), player2_name, gameStatistics(2), gameStatistics(3)); if gameStatistics_errors, printf("[Errors: %d]\n", gameStatistics_errors), end;
+    printf("%s: %d, %s: %d, ties: %d\n", player1_name, gameStatistics(1), player2_name, gameStatistics(2), gameStatistics(3));
+    if gameStatistics_errors, printf("[Errors: %d]\n", gameStatistics_errors), end;
     
     subplot(2,2,1);
     hold on;
@@ -60,7 +52,7 @@ if !isHumanPlaying && gameCount>1
     legend(player1_name, player2_name, 'ties')
     xlabel('Games played')
     ylabel('Games won')
-
+    
     gameStatistics_results ./= ones(3, 1) * sum(gameStatistics_results, 1);
     subplot(2,2,2);
     hold on;
@@ -70,23 +62,11 @@ if !isHumanPlaying && gameCount>1
     legend(player1_name, player2_name, 'ties')
     xlabel('Games played')
     ylabel('Fraction of games won')
-
-    if isequal(player1_name, "playerAI"),
-        global player1_nn_params
-        global player1_nn_lsizes
-        save playerAI/knowledge/1_last player1_nn_lsizes player1_nn_params
-        global player1_learningCurve
-        subplot(2,2,figureID++);
-        plot(player1_learningCurve);
-    end
-
-    if isequal(player2_name, "playerAI"), 
-        global player2_nn_params
-        global player2_nn_lsizes
-        save playerAI/knowledge/2_last player2_nn_lsizes player2_nn_params
-        global player2_learningCurve
-        subplot(2,2,figureID++);
-        plot(player2_learningCurve);
+    
+    if isAIPlaying
+        
+        subplot(2,2,1);
+        %plot();
     end
 end
 endfunction
--- playerAI.m
+++ playerAI.m
@@ -1,32 +1,13 @@
 function gameState = playerAI(gameState, playerNo)
-if playerNo == 1
-    global player1_nn_lsizes
-    global player1_nn_params
-    global player1_lastGameHistory
 
-    if player1_nn_lsizes(1)==9, nn_input = (  zeros(size(gameState)) + (gameState==playerNo) - (gameState==3-playerNo)  )(:)';
-    else if player1_nn_lsizes(1)==18, nn_input = [(gameState==playerNo)(:); (gameState==3-playerNo)(:)]';
-    else if player1_nn_lsizes(1)==27, nn_input = [(gameState==playerNo)(:); (gameState==3-playerNo)(:); (gameState==0)(:)]'; end end end
-    
-    %disp(reshape(nnFeedForward(player1_nn_params, player1_nn_lsizes, nn_input), 3, 3))
+if sum((gameState==0)(:))==0, return, end %Sanity check (don't enter an endless loop if there is no way out)
+while true
+    r=round(rand*(length(gameState(:))-1)+1);
+    if gameState(r)==0
+        gameState(r)=playerNo;
+        break
+    end
+end
 
-    [moveChoice, moveChoice] = max((gameState(:)'==0) .* nnFeedForward(player1_nn_params, player1_nn_lsizes, nn_input));
-    gameState(moveChoice) = playerNo;
-
-    player1_lastGameHistory = [player1_lastGameHistory; nn_input, eye(9)(moveChoice,:)];
-else if playerNo == 2
-    global player2_nn_lsizes
-    global player2_nn_params
-    global player2_lastGameHistory
-
-    if player2_nn_lsizes(1)==9, nn_input = (  zeros(size(gameState)) + (gameState==playerNo) - (gameState==3-playerNo)  )(:)';
-    else if player2_nn_lsizes(1)==18, nn_input = [(gameState==playerNo)(:); (gameState==3-playerNo)(:)]';
-    else if player2_nn_lsizes(1)==27, nn_input = [(gameState==playerNo)(:); (gameState==3-playerNo)(:); (gameState==0)(:)]'; end end end
-    
-    [moveChoice, moveChoice] = max((gameState(:)'==0) .* nnFeedForward(player2_nn_params, player2_nn_lsizes, nn_input));
-    gameState(moveChoice) = playerNo;
-
-    player2_lastGameHistory = [player2_lastGameHistory; nn_input, eye(9)(moveChoice,:)];
-end end
 endfunction
 
