f58ce3918ee211f47f326512ea22821b49825509
Elijas <master.elijas@gmail.com>
2014-12-18 00:30:06 +0200

    added extra features

--- supervisedLearning.m
+++ supervisedLearning.m
@@ -10,14 +10,23 @@ nn_lambda = .0001;
 
 if nn_lsizes(1)==9, LEARNING_INPUT = (LEARNING_INPUT==2) - (LEARNING_INPUT==1);
 else if nn_lsizes(1)==18, LEARNING_INPUT = [(LEARNING_INPUT==2) (LEARNING_INPUT==1)];
-else if nn_lsizes(1)==27, LEARNING_INPUT = [(LEARNING_INPUT==2) (LEARNING_INPUT==1) (LEARNING_INPUT==0)]; end end end
+else if nn_lsizes(1)==27, LEARNING_INPUT = [(LEARNING_INPUT==2) (LEARNING_INPUT==1) (LEARNING_INPUT==0)]; 
+else if nn_lsizes(1)==45
+    features = (LEARNING_INPUT==2) - (LEARNING_INPUT==1);
+    for a=1:8
+        for b=(a+1):9
+            [extra_features(:,i) = [extra_features(:,i) (features(:,a)==features(:,b)&&features(:,a)==2) - (features(:,a)==features(:,b)&&features(:,a)==1)];
+        end
+    end
+    LEARNING_INPUT = [features extra_features];
+end end end end
 
 nn_options = optimset('MaxIter', 150);
 %nn_params = fmincg(@(p) nnCostFunction(p, nn_lsizes, LEARNING_INPUT, LEARNING_OUTPUT, nn_lambda), nnInitParams(nn_lsizes), nn_options);
 
 nn_params = nnInitParams(nn_lsizes);
-nn_alpha = .01;
-for a=1:1
+nn_alpha = .0001;
+for a=1:2
     for b=1:size(LEARNING_INPUT,1)
         printf('%d\r', b); if exist('OCTAVE_VERSION'), fflush(stdout); end
         [J grad] = nnCostFunction(nn_params, nn_lsizes, LEARNING_INPUT(b,:), LEARNING_OUTPUT(b,:), nn_lambda);
